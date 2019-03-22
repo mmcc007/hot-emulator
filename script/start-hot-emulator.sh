bd224c8bf5c92ef883b40d96d2cde489a7ffc5e2
Maurice McCabe <mmcc007@gmail.com>
2019-03-21 17:50:39 -0700

    Simplified emulator startup

--- script/start-hot-emulator.sh
+++ script/start-hot-emulator.sh
@@ -4,7 +4,11 @@
 
 emu_name='test'
 emu_options="-no-audio -no-window -no-boot-anim -gpu swiftshader"
-$ANDROID_HOME/emulator/emulator -avd $emu_name $emu_options &
+nohup $ANDROID_HOME/emulator/emulator -avd $emu_name $emu_options &
+
+# disown emulator process to allow shell to exit
+#emu_pid=$(ps x| grep emulator64-arm| tail -n1 | awk '{print $1}')
+#disown -h $emu_pid
 
 # wait for emulator to start up
-./script/android-wait-for-emulator.sh
+#./script/android-wait-for-emulator.sh
