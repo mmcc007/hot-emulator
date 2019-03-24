13a7beb5f1f42cc291af7979232eb1d69fc4a9d3
Maurice McCabe <mmcc007@gmail.com>
2019-03-23 22:20:35 -0700

    Redirected stdio to prevent hanging on exit from ssh

--- script/start-hot-emulator.sh
+++ script/start-hot-emulator.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-# start emulator from an existing avd with a default snapshot
+# start emulator from an existing avd (with a quickboot snapshot)
+
+# fail on any error
+set -e
 
 emu_name='test'
 emu_options="-no-audio -no-window -no-boot-anim -gpu swiftshader"
-nohup $ANDROID_HOME/emulator/emulator -avd $emu_name $emu_options &
-
-# disown emulator process to allow shell to exit
-#emu_pid=$(ps x| grep emulator64-arm| tail -n1 | awk '{print $1}')
-#disown -h $emu_pid
-
-# wait for emulator to start up
-#./script/android-wait-for-emulator.sh
+# redirect stdin, stdout and stderr to avoid hanging if called from ssh
+/opt/android-sdk/emulator/emulator -avd $emu_name $emu_options > emulator.out 2> emulator.err < /dev/null &
+cat emulator.out
+cat emulator.err
